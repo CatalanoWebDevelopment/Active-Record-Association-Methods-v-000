0a50f3a01caa8b2b905250b8a1056f1f0f5797da
Parker Catalano <catalano.work@gmail.com>
2018-01-19 18:29:30 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,7 +14,9 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    binding.pry
-    self.artists
+    self.artists.collect do |artist|
+      artist.name
+    end
   end
+  
 end
